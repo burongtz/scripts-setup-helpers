ff8ae1ce0287e65621ab21f0bc4018a4a07fbb0f
Javier Buron Gtz <burongtz.javier@gmail.com>
2018-10-25 23:27:14 -0500

    Add method create_conf 	nuevo archivo:  vhost.sh

---
+++ vhost.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# create_conf 'dominio.local' 'domainRoot/frontend/' 'domainRoot/frontend/public'
+
+create_conf() {
+    tags="<VirtualHost *:80>"
+    tags+="\n"
+
+    tags+="\t"
+    tags+="ServerName $1"
+    tags+="\n\t"
+    tags+="ServerAlias www.$1"
+    tags+="\n\t"
+    tags+="ServerAdmin webmaster@localhost"
+
+    tags+="\n\t"
+    tags+="DocumentRoot /var/www/html/$3"
+    tags+="\n\t"
+    tags+="ErrorLog /var/www/html/$2/error.log"
+    tags+="\n\t"
+    tags+="CustomLog /var/www/html/$2/access.log combined"
+
+    tags+="\n\t"
+    tags+="<Directory /var/www/>"
+    tags+="\n\t\t"
+    tags+="Options Indexes FollowSymLinks MultiViews"
+    tags+="\n\t\t"
+    tags+="AllowOverride All"
+    tags+="\n\t\t"
+    tags+="Order allow,deny"
+    tags+="\n\t\t"
+    tags+="allow from all"
+    tags+="\n\t"
+    tags+="</Directory>"
+
+    tags+="\n"
+    tags+="</VirtualHost>"
+    tags+="\n"
+
+
+    echo -e $tags >> /etc/apache2/sites-available/test.conf
+}
+
+# Check if the function exists (bash specific)
+if declare -f "$1" > /dev/null
+then
+  "$@"
+else
+  echo "'$1' is not a known function name" >&2
+  exit 1
+fi
